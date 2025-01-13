ea2c81dd7bd536469cce5a3d858b3eecaed38fdd
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2025-01-12 22:32:51 -0400

    se ha implementado endpoint para reserva de identidad

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,15 @@ import { HttpExceptionFilter } from "./common/filters/all-exceptions.filter";
 import * as fs from "fs";
 dotenv.config(); // Carga las variables de entorno
 async function bootstrap() {
-  /*const httpsOptions = {
+  const httpsOptions = {
     key: fs.readFileSync('/etc/ssl/quickpay.com.bo/private.key'),
     cert: fs.readFileSync('/etc/ssl/quickpay.com.bo/certificate.crt'),
     ca: fs.readFileSync('/etc/ssl/quickpay.com.bo/ca_bundle.crt'), // Si tienes un certificado intermedio
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
-  });*/
-
-
-
-  const app = await NestFactory.create(AppModule);
+  });
+  //const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
 
